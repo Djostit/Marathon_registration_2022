dd47ae10e4fb54e978bb4d1dc713489c3b6cb20f
Djostit <djostit@mail.ru>
2022-04-19 11:43:18 +0500

    Обнова;))

--- Marathon registration/RegestrationPage.xaml.cs
+++ Marathon registration/RegestrationPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MaterialDesignThemes.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace Marathon_registration
 {
     /// <summary>
@@ -23,6 +25,11 @@ namespace Marathon_registration
         public RegestrationPage()
         {
             InitializeComponent();
+        }
+
+        private void PackIcon_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
         }
     }
 }
